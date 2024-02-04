c4ee3752004436b5450a4a9a39ac6a34eddd4cc7
Micah <stalberm@oregonstate.edu>
2024-02-04 11:52:16 -0800

    Remove contact for now

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -30,10 +30,10 @@ const Sidenav = () => {
               <BsPerson size={20} />
               <span className='pl-4'>Resume</span>
             </a>
-            <a onClick={handleNav} href='#contact' className='w-[75%] flex justify-center items-center rounded-full shadow-md bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+            {/* <a onClick={handleNav} href='#contact' className='w-[75%] flex justify-center items-center rounded-full shadow-md bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
               <AiOutlineMail size={20} />
               <span className='pl-4'>Contact</span>
-            </a>
+            </a> */}
           </div>
         ) : (
           ''
@@ -53,9 +53,9 @@ const Sidenav = () => {
           <a href='#main' className='rounded-full shadow-md bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
             <BsPerson size={20} />
           </a>
-          <a href='#contact' className='rounded-full shadow-md bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+          {/* <a href='#contact' className='rounded-full shadow-md bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
             <AiOutlineMail size={20} />
-          </a>
+          </a> */}
         </div>
       </div>
     </div>
